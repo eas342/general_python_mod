b17ce856dffe1c9535c8747828ae2711721812fd
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2016-02-04 19:41:37 -0700

    I added a function to calculate the line number of given text which is useful for the astropy.io.ascii.read() function

--- es_gen.py
+++ es_gen.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 import os
 import sys
+import pdb
 
 @contextmanager
 def suppress_stdout():
@@ -21,5 +22,29 @@ def find_files(name,path):
             result.append(os.path.join(root, name))
     return result, len(result)
     
-#def find_startline():
+def find_startline(filename,phrase,skipblank=False,zerobased=False):
+## Finds the starting line in a text file
+## Useful when data starts after a specific line
+## EXAMPLE
+## headline = es_gen.find_startline(configfile,lookuptext,skipblank=True,zerobased=True)
+## data = ascii.read(configfile,data_start=headline+2,header_start=headline)
+
+
+    if zerobased:
+        startnumber=0
+    else:
+        startnumber=1
+    
+    counter=startnumber
+    with open(filename) as myFile:
+        for num, line in enumerate(myFile,startnumber):
+            if phrase in line:
+                if skipblank:
+                    linenum = counter
+                else:
+                    linenum = num
+            if line != '\n': 
+                counter=counter+1
+           
+    return linenum
     
