d8a263fe1f1acaf4b51665bcc4056b905b2aeef6
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2016-04-21 23:17:00 -0700

    I added a test script to test es_gen.py.

---
+++ test_es_gen.py
@@ -0,0 +1,31 @@
+import es_gen
+
+def test_sigprint(val,sig,desired=None):
+    outval = es_gen.sigprint(val,sig)
+    if outval == desired:
+        print 'Passed'
+    else:
+        print 'Fail:'
+        print "Input: ",val,", nsig= ",sig
+        print "Got: ",outval
+    
+def test_roundfromErr(val,err,desired=None):
+    outval, outerr = es_gen.roundfromErr(val,err)
+    if (outval == desired[0]) and (outerr == desired[1]):
+        print 'Passed'
+    else:
+        print 'Fail:'
+        print "Input: ",val," +/- ",err
+        print "Got: ",outval," +/- ",outerr
+        print "Desired: ",desired[0]," +/- ",desired[1]
+    
+if __name__ == "__main__":
+    
+    test_sigprint(123.44738,2,desired="120")
+    test_sigprint(0,2,desired="0.0")
+    test_roundfromErr(222.23,121.443,desired=["220","120"])
+    test_roundfromErr(1432.3,77,desired=["1432","77"])
+    test_roundfromErr(-1432.3,77,desired=["-1432","77"])
+    test_roundfromErr(0,0.773422,desired=["0.00","0.77"])
+    test_roundfromErr(2.5,0.3,desired=["2.50","0.30"])
+    
