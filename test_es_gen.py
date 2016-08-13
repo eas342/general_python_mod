396d92556ccf7be6266630661927658c9258113a
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2016-08-13 13:48:23 -0700

    I adjusted the test_es_gen script to account for the move of these modules to my corner.py.

--- test_es_gen.py
+++ test_es_gen.py
@@ -1,16 +1,17 @@
-import es_gen
+import corner
 
 def test_sigprint(val,sig,desired=None):
-    outval = es_gen.sigprint(val,sig)
+    outval = corner.sigprint(val,sig)
     if outval == desired:
         print 'Passed'
     else:
         print 'Fail:'
         print "Input: ",val,", nsig= ",sig
         print "Got: ",outval
+        print "Desired: ",desired
     
 def test_roundfromErr(val,err,desired=None):
-    outval, outerr = es_gen.roundfromErr(val,err)
+    outval, outerr = corner.roundfromErr(val,err)
     if (outval == desired[0]) and (outerr == desired[1]):
         print 'Passed'
     else:
@@ -23,6 +24,8 @@ if __name__ == "__main__":
     
     test_sigprint(123.44738,2,desired="120")
     test_sigprint(0,2,desired="0.0")
+    test_sigprint(0.9999,2,desired="1.0")
+    test_sigprint(1e5,2,desired="1.0e+05")
     test_roundfromErr(222.23,121.443,desired=["220","120"])
     test_roundfromErr(1432.3,77,desired=["1432","77"])
     test_roundfromErr(-1432.3,77,desired=["-1432","77"])
